d1b4ed53cbec1518c2360f1771b7561d3da25e2d
Jun Gao <0618johnny@gmail.com>
2022-12-26 19:47:47 -0800

    Update KalmanFilter.py

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -1,4 +1,40 @@
 class KalmanFilter:
+    """
+    A simple Kalman filter implementation.
+    
+    Assumes a process with additive white Gaussian noise and attempts to estimate
+    the state of the process using noisy measurements.
+    
+    Parameters
+    ----------
+    process_variance : float
+        The variance of the process noise. Controls the trade-off between trusting
+        the process model and the measurements.
+    estimated_measurement_variance : float
+        The variance of the measurement noise. Controls the trade-off between trusting
+        the process model and the measurements.
+        
+        
+    Example
+    ----------
+        # Create a Kalman filter with some process variance and some estimated measurement variance
+        kalman_filter = KalmanFilter(1, 1)
+
+        # Provide an initial estimate of the process state
+        kalman_filter.input_latest_noisy_measurement(5)
+
+        # Get the current estimate of the process state
+        estimate = kalman_filter.get_latest_estimated_measurement()
+        print(estimate)  # Output: 5
+
+        # Input a new measurement
+        kalman_filter.input_latest_noisy_measurement(10)
+
+        # Get the updated estimate of the process state
+        estimate = kalman_filter.get_latest_estimated_measurement()
+        print(estimate)  # Output: something close to 7.5
+    """
+    
     def __init__(self, process_variance, estimated_measurement_variance):
         self.process_variance = process_variance
         self.estimated_measurement_variance = estimated_measurement_variance
@@ -6,6 +42,14 @@ class KalmanFilter:
         self.posteri_error_estimate = 1.0
 
     def input_latest_noisy_measurement(self, measurement):
+        """
+        Update the estimate of the process state based on the latest measurement.
+        
+        Parameters
+        ----------
+        measurement : float
+            The latest measurement of the process state.
+        """
         priori_estimate = self.posteri_estimate
         priori_error_estimate = self.posteri_error_estimate + self.process_variance
 
@@ -14,4 +58,12 @@ class KalmanFilter:
         self.posteri_error_estimate = (1 - blending_factor) * priori_error_estimate
 
     def get_latest_estimated_measurement(self):
+        """
+        Return the current estimate of the process state.
+        
+        Returns
+        -------
+        float
+            The current estimate of the process state.
+        """
         return self.posteri_estimate
